5459b51049fcff9203a90b578fda0ebbbcf1dcab
Rich Schiffer <richschiffer@Richs-MacBook-Pro.local>
2018-06-05 16:07:33 -0600

    most css done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 var wins = 0;
 var losses = 0;
-var userguess = [];
-var lives = 10;
+var userGuess = [];
+var lives = 9;
 var win_span = document.getElementById("winstotal");
 var loss_span = document.getElementById("losstotal");
 var guessLeft_span = document.getElementById("current-guesses");
@@ -10,10 +10,12 @@ var sofar_span = document.getElementById("past-guesses");
 var letterArray = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
 var mysteryLetter = letterArray[Math.floor(Math.random() * letterArray.length)];
 
+// resets the game while keeping track of wins/losses
 function nextGame (){
-    lives = 10;
+    lives = 9;
     guessLeft_span.innerHTML = lives;
-    userguess = [];
+    sofar_span.innerHTML = "";
+    userGuess.length = 0;
     mysteryLetter = letterArray[Math.floor(Math.random() * letterArray.length)];
     console.log(mysteryLetter);
 }
@@ -21,25 +23,34 @@ function nextGame (){
 // allows user to press a letter to make a guess
 document.onkeyup = function(event) {
     var guess = event.key.toLowerCase();
+    var alreadyUsedGuess = userGuess.includes(" " + guess);
+    
+    //alert user on repeat guesses
+    if (alreadyUsedGuess == true) {
+        alert("You already guessed that letter... you might want to try a different one!");
 
-    //alter user if they make an invalid guess
-    if(!letterArray.includes(guess)) {
+    //alert user if they make an invalid guess
+    } else if (!letterArray.includes(guess)) {
         alert("Invalid key pressed. You must choose a letter!");
-        
-    } else {
-        userguess.push(" " + guess);
-        
+
+    // creates spacing between letters and commas
+    } else { 
+        userGuess.push(" " + guess);
+
+        // win condition
         if (guess === mysteryLetter) {
             win_span.innerHTML++;
             nextGame();
-            
+
+        // lose condition      
         } else if(lives <= 1) {
             alert("You lose. Better luck next time!");
             loss_span.innerHTML++;
             nextGame();
 
+        // tracks guesses/guesses left and decrements
         } else {
-            sofar_span.innerHTML = userguess;
+            sofar_span.innerHTML = userGuess;
             guessLeft_span.innerHTML--;
             lives--;
         }
